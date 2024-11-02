c3dcd8c1ae389e4f6a5e1ab061703f9ac0961bc4
Any Gabriely <129224785+any-g@users.noreply.github.com>
2024-11-01 21:24:58 -0300

    Update logar.py
    add de flash message e pagina de erro

--- anyyy/aulatreino/controllers/logar.py
+++ anyyy/aulatreino/controllers/logar.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, Blueprint, redirect, session, url_for
+from flask import Flask, render_template, request, Blueprint, redirect, session, url_for, flash
 
 loginController = Blueprint("login", __name__)
 
@@ -6,16 +6,23 @@ loginController = Blueprint("login", __name__)
 
 @loginController.route('/')
 def logar():
+    
     return render_template('login.html')
 
+@app.errorhandler(naocadastro)
+def (e):
+    return render_ template ("erro.html", message = "Não está cadastrado!"), 500
+    
 @loginController.route("/verificar", method=["POST, GET"])
 def verificar():
     nome = request.form["nome"]
     senha = request.form["senha"]
     if senha==1234 and nome =="Any":
         session['nome']=nome
-
+        flash.sucess("Login de sucesso!")
         return render_template('logarr.html')
+    flash.error("Algo deu errado!")
+    abort(401)
     return 'Usuário não cadastrado'
 
 @loginController.route('/login')
