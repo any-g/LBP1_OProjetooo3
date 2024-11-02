305f86bfc565709acc6d7ae694d2af30b69dbbb9
Any Gabriely <129224785+any-g@users.noreply.github.com>
2024-11-01 21:53:25 -0300

    Delete projeto_foda directory

--- projeto_foda/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, redirect, url_for, request, session
-from controllers.login import login_control
-from controllers.controller import any_controller
-app = Flask(__name__)
-app.secret_key = 'chave_screta'
-app.register_blueprint(login_control)
-
-if __name__ == "__main__":
-    app.run()
